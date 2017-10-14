e562d55b8a1708bd3b37c17c22dfdcfa89795e30
msiddons <gny17hvu@uea.ac.uk>
2017-10-14 20:01:33 +0100

    renamed loop variable to row

--- Random Stars/Source.cpp
+++ Random Stars/Source.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int main()
 {
-	int count = 0, loop = 1, rnd; //rnd1, rnd2;
+	int count = 0, row = 1, rnd;
 	srand(time(NULL)); //set the seed for your random number to be the current time
-	while (loop < 21) // output 20 lines
+	while (row < 21) // output 20 lines
 	{
 		rnd = rand() % 8 + 1; //assign random number between 1 and 8
 		while (count < rnd)
@@ -15,7 +15,7 @@ int main()
 			cout << "*";
 			count++;
 		}
-		loop++;
+		row++;
 		count = 0;
 		cout << endl;
 	}
